06a3457496167f2275717bf1a4465ca638eac837
Vincent Whales <vincent@whales.media>
2018-08-23 21:19:17 -0700

    Tutorial 1

---
+++ snippets/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from snippets.models import Snippet
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
